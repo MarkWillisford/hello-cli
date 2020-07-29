21fd5ae88ddc517f4ee776e0d2b1c25c636e3d84
Mark Willisford <MarkWillisford@users.noreply.github.com>
2020-07-28 17:56:26 -0700

    adding search

--- bin/index.js
+++ bin/index.js
@@ -19,16 +19,32 @@ const msgBox = boxen( greeting, boxenOptions );
 const options = yargs
   .usage("Usage: -n <name>")
   .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+  .option("s", { alias: "search", describe: "Search term", type: "string" })
   .argv;
 
 const greetingYargStyle = `Hello, ${options.name}!`;
-
 console.log(greetingYargStyle);
-console.log("Here's a random joke for you:");
 
-const url = "https://icanhazdadjoke.com/";
+if(options.search){
+  console.log(`Searching for jokes about ${options.search}...`)
+} else {
+  console.log("Here's a random joke for you:");
+}
+
+// The url depends on searching or not
+const url = options.search ? `https://icanhazdadjoke.com/search?term=${escape(options.search)}` : "https://icanhazdadjoke.com/";
 
 axios.get(url, { headers: {Accept: "application/json" } })
   .then(res => {
-    console.log(res.data.joke);
-  })
+    if(options.search){
+      // if searching for jokes, loop over results
+      res.data.results.forEach( j => {
+        console.log("\n" + j.joke);
+      });
+      if(res.data.results.length === 0){
+        console.log("no jokes found :'(");
+      }
+    } else {
+      console.log(res.data.joke);
+    }
+  });
